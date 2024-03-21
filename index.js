5c0919bb25ac0a23d309bf6314a412ba7a1000eb
Anton Angeletti <angelettianton@gmail.com>
2024-03-21 16:26:03 -0500

    wooo clyde

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 import { config } from 'dotenv'
-import { Client, Events, GatewayIntentBits, REST, Routes } from 'discord.js'
+import { Client, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder } from 'discord.js'
 
 config()
 if (!process.env.BOT_TOKEN || !process.env.OPENAI_KEY || !process.env.OPENAI_URL) {
   throw new Error('fix ur .env homie')
 }
 
-const commands = [
-  {
-    name: 'clyde',
-    description: 'clyde',
-  },
-]
+async function askClyde(question) {
+  return "back in 'nam, I killed some kids idk"
+}
+
+// const clydeCommand = new SlashCommandBuilder()
+//   .setName('clyde')
+//   .setDescription('CLYDE')
+//   .addStringOption((option) => option.setName('prompt'))
+
+const clydeCommand = {
+  name: 'clyde',
+  description: 'CLYDE!',
+  options: [
+    {
+      type: 3,
+      name: 'prompt',
+      description: 'CLYDE!',
+      required: true,
+    },
+  ],
+}
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN)
 try {
   console.log('Started refreshing application (/) commands.')
-
-  await rest.put(Routes.applicationCommands(process.env.BOT_ID), { body: commands })
-
+  await rest.put(Routes.applicationCommands(process.env.BOT_ID), { body: [clydeCommand] })
   console.log('Successfully reloaded application (/) commands.')
 } catch (error) {
   console.error(error)
@@ -34,7 +47,14 @@ client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return
 
   if (interaction.commandName === 'clyde') {
-    await interaction.reply('CLYDE!')
+    const prompt = console.log(interaction.options.getString('prompt'))
+    let response = 'idk lol'
+    try {
+      response = await askClyde(prompt)
+    } catch (error) {
+      console.error(error)
+    }
+    await interaction.reply(response)
   }
 })
 
